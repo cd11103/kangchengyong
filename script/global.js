ed8f200380c0d494956e248919eafaa47c6c17df
cd11103 <cheng.yong.kang@iotadev.local>
2017-01-15 21:33:15 +0800

    9

--- script/global.js
+++ script/global.js
@@ -1,14 +1,3 @@
-var domain = "http://localhost/Locum/";
-//var domain = "https://ess.knowledgetouch.com/";
-
-var orgType = "DEMO";
-//var SUCCESS = "100";
-
-function logout(){
-    var logout = confirm("Are you sure you want to logout?");
-    if (logout == true) {
-        window.localStorage.clear();
-        window.location.href='index.html';
-    } 
-    
-}
+jQuery(document).ready(function(){
+	
+});
